7d57df42ad21f9d5444278b0d9ca9c4581635e3a
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2018-06-02 21:15:43 +0530

    Add files via upload

---
+++ snap.py
@@ -0,0 +1,13 @@
+import time
+import cv2
+from PIL import Image
+
+camera_port = 0
+camera = cv2.VideoCapture(camera_port)
+time.sleep(0.1)  # If you don't wait, the image will be dark
+return_value, image = camera.read()
+cv2.imwrite("click.png", image)
+img = Image.open('click.png')
+img.show()
+del(camera)  # so that others can use the camera as soon as possible
+
