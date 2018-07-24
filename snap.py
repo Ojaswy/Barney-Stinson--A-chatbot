e9920ef3e9c5f8c3e626c2eb22f77559e9a904f9
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2018-07-24 20:47:25 +0530

    Update snap.py

--- snap.py
+++ snap.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 camera_port = 0
 camera = cv2.VideoCapture(camera_port)
-time.sleep(0.1)  # If you don't wait, the image will be dark
+time.sleep(0.1)  #Time for capture, else image will be dark
 return_value, image = camera.read()
 cv2.imwrite("click.png", image)
 img = Image.open('click.png')
